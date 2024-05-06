baca19f9e86b70f9b1a752ee073ad410fa474e38
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2024-05-06 09:14:29 +0300

    Add files via upload

---
+++ kasutajad.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: d123176.mysql.zonevs.eu
+-- Loomise aeg: Mai 06, 2024 kell 09:14 EL
+-- Serveri versioon: 10.4.32-MariaDB-log
+-- PHP versioon: 8.2.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Andmebaas: `d123176_andmebaas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `kasutajad`
+--
+
+CREATE TABLE `kasutajad` (
+  `id` int(11) NOT NULL,
+  `kasutajanimi` varchar(255) NOT NULL,
+  `parool` varchar(255) NOT NULL,
+  `roll` enum('admin','tavakasutaja') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `kasutajad`
+--
+
+INSERT INTO `kasutajad` (`id`, `kasutajanimi`, `parool`, `roll`) VALUES
+(1, 'admin', 'adminpassword', 'admin'),
+(2, 'user', 'userpassword', 'tavakasutaja'),
+(3, 'Larmesdesang', 'Larmesdesang123.', 'admin'),
+(5, 'Pirog', 'Pirog123.', 'tavakasutaja'),
+(6, 'irina', 'irina5.', 'tavakasutaja');
+
+--
+-- Indeksid tõmmistatud tabelitele
+--
+
+--
+-- Indeksid tabelile `kasutajad`
+--
+ALTER TABLE `kasutajad`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `kasutajanimi` (`kasutajanimi`);
+
+--
+-- AUTO_INCREMENT tõmmistatud tabelitele
+--
+
+--
+-- AUTO_INCREMENT tabelile `kasutajad`
+--
+ALTER TABLE `kasutajad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
